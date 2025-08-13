664ee9f6ca314c2c748da1ba30db5dc5d13e822f
Laxmi Gupta <23030522@quantumuniversity.edu.in>
2025-08-13 06:10:48 +0530

    Add about section

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm <b>Laxmi Gupta</b>, a passionate developer and lifelong learner. 
+        Welcome to my colorful and creative online portfolio. 
+        Here you’ll find a showcase of my skills, projects, and resume.
+      </p>
+    </section>
+  );
+}
